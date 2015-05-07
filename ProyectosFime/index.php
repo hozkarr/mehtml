2c8c8f55bc7435b4c1bfaa6f61adbb647129ba86
OscarGarciaRuiz <oscar.luis.garcia94@gmail.com>
2015-05-07 09:26:07 -0500

    diseño

--- ProyectosFime/index.php
+++ ProyectosFime/index.php
@@ -1,25 +1,36 @@
 <?php
 
-mysql_connect("localhost", "root", "kirby94") or die (mysql_error());
-mysql_select_db('google+') or die(mysql_error());
-
-
 require 'vendor/autoload.php';
 $faker = Faker\Factory::create();
 
-
+$name = array();
+$last = array();
+$date  =array();
 
 for ($i = 0; $i <= 20; $i++) {
 
-    echo $i;
-     $name = $faker->name;
-     $last = $faker->lastName;
-     $comment = $faker->text;
-     $date = $faker->dateTimeBetween($startDate = "now", $endDate = "30 days")->format('Y-m-d H:i:s')."<br>";
+     $name[] = $faker->name;
+
+     $last[] = $faker->lastName;
+
+     $date[] = $faker->dateTimeBetween($startDate = "now", $endDate = "30 days")->format('Y-m-d H:i:s');
 
     /*mysql_query("INSERT INTO `google+`.`post` (`id`, `name`, `last`, `comment`, `date`)
             VALUES (NULL, '$name', '$last', '$comment', '$date');") or die (mysql_error());*/
 }
 
-//var_dump(get_object_vars($date));
+echo "<table>";
+echo"<form action='injection.php' method='post'>";
+echo"<tr><td>Nombre</td><td>Apellido</td><td>Comentario</td><td>Fecha</td>";
 
+for ($i = 0; $i <= 20; $i++) {
+
+    echo"<tr><td><input type='text' name='name[]' id='name' value='".$name[$i]."'readonly ></td>";
+    echo"<td><input type='text' name='last[]' id='name' value='".$last[$i]."'readonly ></td>";
+    echo"<td><input type='text' name='text[]' id='name'></td>";
+    echo"<td><input type='text' name='date[]' id='name' value='".$date[$i]."'readonly ></td>";
+    echo"<td><input type='submit' value='Enviar' ></td></tr>";
+
+}
+
+echo "</form></table>";
